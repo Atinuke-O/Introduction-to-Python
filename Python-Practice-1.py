8c2a0f8fccbc13053ab8b2c49ae342b5f6deeabd
Atinuke-O <67044333+Atinuke-O@users.noreply.github.com>
2020-07-10 22:58:24 -0400

    Update Python-Practice-1.py

--- Python-Practice-1.py
+++ Python-Practice-1.py
@@ -1,34 +1,34 @@
-# [ ] print your name
+# print your name
 print("Atinuke")
 
-# [ ] print "is using Python"
+# print "is using Python"
 print("is using Python!")
 
-# [ ] create a variable your_name and assign it a string containing your name
+# create a variable your_name and assign it a string containing your name
 your_name=("Atinuke G Oyinbo")
 
-# [ ] print your_name
+# print your_name
 print(your_name)
 
-# [ ] create variables and assign values for: favorite_song, shoe_size, lucky_number
+# create variables and assign values for: favorite_song, shoe_size, lucky_number
 
 favorite_song=("The One Who Never Comes")
 shoe_size=("11")
 lucky_number=("7")
 
-# [ ] print the value of each variable favorite_song, shoe_size, and lucky_number
+# print the value of each variable favorite_song, shoe_size, and lucky_number
 
 print(favorite_song)
 print(shoe_size)
 print(lucky_number)
 
-# [ ] print favorite_song with description
+# print favorite_song with description
 print ("My favorite song is", favorite_song, "by Asa")
 
-# [ ] print shoe_size with description
+# print shoe_size with description
 print ("I have a very big shoe size of", shoe_size)
 
-# [ ] print lucky_number with description
+# print lucky_number with description
 print ("I've always loved the number", lucky_number)
 
 # assign favorite_lucky_shoe using
@@ -36,7 +36,7 @@ print ("I've always loved the number", lucky_number)
 favorite_lucky_shoe = (favorite_song + " will never be a person that hates the number " + lucky_number + " and wears a size " + shoe_size)
 print(favorite_lucky_shoe)
 
-# [ ] print a diagonal using "*"
+# print a diagonal using "*"
 
 print("*")
 print(" *")
@@ -44,14 +44,14 @@ print("  *")
 print("   *")    
 print("    *")
 
-# [ ] rectangle using "*"
+# rectangle using "*"
 
 print("***********")
 print("*         *")
 print("*         *")
 print("***********")
 
-# [ ] smiley using "*"
+# smiley using "*"
 
 print("*         *")
 print("     *     ")
@@ -59,7 +59,7 @@ print("*         *")
 print(" *       * ")
 print("  *******  ")
 
-# [ ] create integer variables (x, y, z) and assign them 1-3 digit integers (no decimals - no quotes)
+# create integer variables (x, y, z) and assign them 1-3 digit integers (no decimals - no quotes)
 
 x=(43)
 y=(46)
@@ -67,7 +67,7 @@ z=(50)
 xyz_sum=(x + y + z)
 print(xyz_sum)
 
-# [ ] ASCII ART
+# ASCII ART
 
 print("_         _")
 print(" \_(^-^)_/")
