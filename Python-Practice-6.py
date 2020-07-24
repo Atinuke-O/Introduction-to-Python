dd602648f36cba31a2aea0eda77b4b06794cedbc
Atinuke-O <67044333+Atinuke-O@users.noreply.github.com>
2020-07-24 01:33:11 -0400

    Update and rename Python-Practice-6 to Python-Practice-6.py

---
+++ Python-Practice-6.py
@@ -0,0 +1,14 @@
+    #-------Nested Conditionals-------#
+
+# print a string that outputs the following exactly: The new line character is "\n"
+print("The new line character is \"\\n\"")
+
+# print output that is exactly (with quotes): "That's how we escape!"
+print("\"That's how we escape!\"")
+
+# with only 1 print statement and using No Space Characters, output the text commented below  
+
+# 1       one
+# 22      two
+# 333     three
+print("1\tone\n22\ttwo\n333\tthree")
