6c49ec0feb08f26f10fb9ebad1023f2b329d3d33
Atinuke-O <67044333+Atinuke-O@users.noreply.github.com>
2020-07-26 00:38:03 -0400

    Update Python-Practice-4.py

--- Python-Practice-4.py
+++ Python-Practice-4.py
@@ -23,3 +23,23 @@ if number.isdigit():
 else:
     print ("only int is accepted")
 # It appears that there must always be a variable involved and the int() and str() doesn't work well with input() alone
+
+#-------Pet Conversation-------#
+
+#**ask the user for a sentence about a pet and then reply**  
+#- get user input in variable: about_pet
+#- using a series of **if** statements respond with appropriate conversation
+  #- check if "dog" is in the string about_pet (sample reply "Ah, a dog")
+  #- check if "cat" is in the string about_pet
+  #- check if 1 or more animal is in string about_pet
+#- finish with thanking for the story
+
+
+about_pet = (input ("Start a conversation about a pet: "))
+
+if "dog" in about_pet:
+    print ("Ah, a dog. how wonderful! Dogs are amazing")
+if "cat" in about_pet:
+    print ("Cats are very sassy creatures. But they are good cuddlers ^-^")
+if "dog, cat, hamster, guinea pig, turtle, goldfish, bunny" in animals:
+    print ("It's so great to have lots of animal company!")
